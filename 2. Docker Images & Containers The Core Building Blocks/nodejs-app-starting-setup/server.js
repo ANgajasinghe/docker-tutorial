1114f0d48aa35e968e45d3d6fbe40c7c25d4b933
ANgajasinghe <Akalanka Nayanajith@DESKTOP-TERE1H0>
2021-04-25 14:16:27 +0530

    Optimization 01

--- 2. Docker Images & Containers The Core Building Blocks/nodejs-app-starting-setup/server.js
+++ 2. Docker Images & Containers The Core Building Blocks/nodejs-app-starting-setup/server.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
       </head>
       <body>
         <section>
-          <h2>My Course Goal</h2>
+          <h2>My Course Goal!!!!!</h2>
           <h3>${userGoal}</h3>
         </section>
         <form action="/store-goal" method="POST">
