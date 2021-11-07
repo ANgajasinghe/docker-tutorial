8958cf2a2ab1938e466f848f65d06c93c5697765
ANgajasinghe <angajasinghe@gmail.com>
2021-11-07 19:21:42 +0530

    Add emptyDir volume

---
+++ 10. Kubernetes Volume/app.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const fs = require('fs');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const filePath = path.join(__dirname, 'story', 'text.txt');
+
+app.use(bodyParser.json());
+
+app.get('/story', (req, res) => {
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      return res.status(500).json({ message: 'Failed to open file.' });
+    }
+    res.status(200).json({ story: data.toString() });
+  });
+});
+
+app.post('/story', (req, res) => {
+  const newText = req.body.text;
+  if (newText.trim().length === 0) {
+    return res.status(422).json({ message: 'Text must not be empty!' });
+  }
+  fs.appendFile(filePath, newText + '\n', (err) => {
+    if (err) {
+      return res.status(500).json({ message: 'Storing the text failed.' });
+    }
+    res.status(201).json({ message: 'Text was stored!' });
+  });
+});
+
+app.get('/error', ()=> process.exit(1));
+
+
+app.listen(3000);
