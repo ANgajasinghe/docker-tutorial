dce0df6c709d51ac98e8f4be48b281e145a9039f
ANgajasinghe <angajasinghe@gmail.com>
2021-11-08 07:17:13 +0530

    AUTH_SERVICE_SERVICE_HOST - automatically generated ip address

--- 11. Kubernetes Networking/users-api/users-app.js
+++ 11. Kubernetes Networking/users-api/users-app.js
@@ -23,7 +23,7 @@ app.post('/signup', async (req, res) => {
   }
 
   try {
-    const hashedPW = await axios.get('http://'+ process.env.AUTH_ADDRESS +'/hashed-password/' + password);
+    const hashedPW = await axios.get('http://'+ process.env.AUTH_SERVICE_SERVICE_HOST +'/hashed-password/' + password);
     // since it's a dummy service, we don't really care for the hashed-pw either
     console.log(hashedPW, email);
     res.status(201).json({ message: 'User created!' });
@@ -54,7 +54,7 @@ app.post('/login', async (req, res) => {
   // normally, we'd find a user by email and grab his/ her ID and hashed password
   const hashedPassword = password + '_hash';
   const response = await axios.get(
-    'http://'+ process.env.AUTH_ADDRESS +'/token/' + hashedPassword + '/' + password
+    'http://'+ process.env.AUTH_SERVICE_SERVICE_HOST +'/token/' + hashedPassword + '/' + password
   );
   if (response.status === 200) {
     return res.status(200).json({ token: response.data.token });
