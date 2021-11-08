3c43d118d559d0ea8181f18cbd2800a86835b161
ANgajasinghe <angajasinghe@gmail.com>
2021-11-08 06:24:29 +0530

    Volume and ENV

--- 10. Kubernetes Volume/app.js
+++ 10. Kubernetes Volume/app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const filePath = path.join(__dirname, 'story', 'text.txt');
+const filePath = path.join(__dirname, process.env.STORY_FOLDER || 'story', 'text.txt');
 
 app.use(bodyParser.json());
 
